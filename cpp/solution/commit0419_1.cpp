628dc1ed5dd16bbf92ab7e9f8e42e174e1717c0e
syj <1171871069@qq.com>
2020-04-19 12:49:18 +0800

    update

--- cpp/solution/commit0419_1.cpp
+++ cpp/solution/commit0419_1.cpp
@@ -1,71 +1,50 @@
 //
 // Created by syj on 2020/4/18.
 // 修改了图的存储结构
-// 在commit0418_1的基础上修改了vertex_set为int，没有报错，说明vertex都是int
-// 7.7371
+// 在commit0418_2的基础上修改了图的结构，顶点不用哈希。。。
+// 6.8813
 //
 #include <iostream>
 #include <list>
 #include <vector>
 #include <string>
 #include <set>
-#include <deque>
-#include <unordered_map>
 
-#pragma comment(linker, "/STACK:10000000")
+#pragma comment(linker, "/STACK:1000000000")
 
 using namespace std;
 
 class FindCycleSolution {
 public:
     FindCycleSolution() {
-        cycle_num = 0, total_vertex = 0;
+        cycle_num = 0;
         result[0].resize(3 * 500000);
         result[1].resize(4 * 500000);
         result[2].resize(5 * 1000000);
         result[3].resize(6 * 2000000);
         result[4].resize(7 * 3000000);
+        graph = new set<int>[500000];
+        reverse_graph = new set<int>[500000];
+        visited = new bool[500000]{false};
     }
 
     ~FindCycleSolution() = default;
 
     void generate_graph(string &file_path) {
-        unordered_map<unsigned int, int> vertex_hash;
         FILE *file = fopen(file_path.c_str(), "r");
         unsigned int from, to, weight;
         vector<pair<unsigned int, unsigned int>> input_pair;
         while (fscanf(file, "%u,%u,%u", &from, &to, &weight) != EOF) {
             input_pair.emplace_back(from, to);
-            vertex_set.push_back(to);
-            vertex_set.push_back(from);
-        }
-        fclose(file);
-        set<unsigned int> st(vertex_set.begin(), vertex_set.end());
-        vertex_set.assign(make_move_iterator(st.begin()), make_move_iterator(st.end()));
-
-        for (auto &vertex : vertex_set) {
-            vertex_hash[vertex] = total_vertex++;
-        }
-
-        graph = new set<int>[total_vertex];
-        reverse_graph = new vector<bool>[total_vertex];
-        visited = new bool[total_vertex]{false};
-
-        int f, t;
-        for (int i = 0; i < total_vertex; i++) {
-            reverse_graph[i].resize(total_vertex, false);
-        }
-
-        for (auto &pair : input_pair) {
-            f = vertex_hash[pair.first], t = vertex_hash[pair.second];
-            graph[f].insert(t);
-            reverse_graph[t][f] = true;
+            vertex_set.insert(to);
+            vertex_set.insert(from);
+            graph[from].insert(to);
+            reverse_graph[to].insert(from);
         }
     }
 
-
     void findCyclesInScc() {
-        for (int i = 0; i < total_vertex; ++i) {
+        for (auto &i : vertex_set) {
             findAllSimpleCycles(i, i, 0);
         }
     }
@@ -77,7 +56,7 @@ public:
         for (auto &adjacent : graph[current]) {
             if (adjacent == start && depth > 1) {
                 for (int i = 0; i < depth + 1; ++i) {
-                    result[depth - 2][((depth + 1) * result_index[depth - 2]) + i] = vertex_set[pointStack[i]];
+                    result[depth - 2][((depth + 1) * result_index[depth - 2]) + i] = pointStack[i];
                 }
                 ++result_index[depth - 2];
                 ++cycle_num;
@@ -85,13 +64,16 @@ public:
                 if (depth < 5) {
                     findAllSimpleCycles(start, adjacent, depth + 1);
                 } else if (depth == 5) {
-                    if (reverse_graph[start][adjacent]) {
-                        pointStack[depth + 1] = adjacent;
-                        for (int i = 0; i < depth + 2; ++i) {
-                            result[depth - 1][((depth + 2) * result_index[depth - 1]) + i] = vertex_set[pointStack[i]];
+                    for (auto &v:reverse_graph[start]) {
+                        if (v == adjacent) {
+                            pointStack[depth + 1] = adjacent;
+                            for (int i = 0; i < depth + 2; ++i) {
+                                result[depth - 1][((depth + 2) * result_index[depth - 1]) + i] = pointStack[i];
+                            }
+                            ++result_index[depth - 1];
+                            ++cycle_num;
+                            break;
                         }
-                        ++result_index[depth - 1];
-                        ++cycle_num;
                     }
                 }
             }
@@ -119,13 +101,11 @@ public:
 private:
     bool *visited;
     int pointStack[7];
-    vector<unsigned int> result[5];
+    vector<int> result[5];
     int result_index[5]{};
 
-    vector<int> vertex_set;
-    set<int>* graph;
-    vector<bool> *reverse_graph;
-    int total_vertex;
+    set<int> vertex_set;
+    set<int> *graph, *reverse_graph;
     int cycle_num;
 };
 
@@ -133,9 +113,9 @@ int main() {
 //    string data_path = R"(D:\hw\data\test_data.txt)";
 //    string linux_path = R"(/home/syj/Documents/hw/data/test_data.txt)";
 //    string huawei_path = R"(/root/hw/data/test_data.txt)";
-//    string o = "result.txt";
-    string iPath = "/data/test_data.txt";
+//    string oPath = "result.txt";
     string oPath = "/projects/student/result.txt";
+    string iPath = "/data/test_data.txt";
     FindCycleSolution solution;
     solution.generate_graph(iPath);
     solution.findCyclesInScc();
